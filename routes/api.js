2ca8e25110438856d4c80fd70283172d059549d3
Glitch (yummy-packet) <none>
2019-02-09 09:40:24 +0000

    ☠️🎩 Checkpoint ./routes/api.js:2619925/820

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,34 @@
 'use strict';
 
 var expect = require('chai').expect;
-var 
+var mongoClient = require('mongodb').MongoClient
+const CONN = process.env.DB
+var DB
+
+mongoClient.connect(CONN, (err, dba) => {
+  if (err)
+    console.log('Connection Failure')
+  else
+    console.log('Connection Successful')
+    DB = dba
+})
+
+
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    
+    .post((req, res) => {
+      let board = req.params.board
+      let newThread = req.body //{"board":"hero","text":"This is stupid","delete_password":"black"}
+      
+      newThread['created_on'] = new Date()
+      newThread['bumped_on'] = new Date()
+      
+      // db.collection(board).insert()
+      
+    })
     
   app.route('/api/replies/:board');
 
